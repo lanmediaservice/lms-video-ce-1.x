0f615b1d03cc548a44e11de807210f359de49b0b
Илья Спесивцев <macondos@gmail.com>
2011-09-17 11:38:02 +0300

    исправления

---
+++ src/app/tasks/batch-daily.bat
@@ -0,0 +1,4 @@
+php.bat indexing.php
+php.bat ranking.php
+php.bat suggestion-cache.php
+php.bat bestsellers.php
--- src/app/tasks/batch-daily.php
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/php -q
-<?php
-passthru(dirname(__FILE__) . '/indexing.php');
-passthru(dirname(__FILE__) . '/ranking.php');
-passthru(dirname(__FILE__) . '/suggestion-cache.php');
-passthru(dirname(__FILE__) . '/bestsellers.php');
